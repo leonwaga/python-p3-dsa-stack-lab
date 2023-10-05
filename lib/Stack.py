9dc6482c0cffc789156ec9b2e82ee819c31641ff
leon <leon.waga@student.moringaschool.comgit config --global user.name leongit config --global user.name leon>
2023-10-06 01:18:50 +0300

    completed assignment

--- lib/Stack.py
+++ lib/Stack.py
@@ -2,24 +2,43 @@ class Stack:
 
     def __init__(self, items = [], limit = 100):
         pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
         pass
-
+        return len(self.items) == 0
+    
     def push(self, item):
         pass
-
+        if not self.full():
+            self.items.append(item) 
+    
     def pop(self):
-        pass
+        if not self.isEmpty():
+            item = self.items[-1]
+            self.items.pop()
+            return item
 
     def peek(self):
-        pass
+        if not self.isEmpty():
+            item = self.items[-1]
+
+        return len(self.items)
     
     def size(self):
-        pass
-
+       return len(self.items)
+    
     def full(self):
-        pass
+        return self.limit == self.size()
 
     def search(self, target):
-        pass
+        distance = -1
+        index = self.size()-1
+        while index >=0:
+            distance +=1
+            if target == self.items[index]:
+                return distance
+            index -= 1
+
+        return -1
